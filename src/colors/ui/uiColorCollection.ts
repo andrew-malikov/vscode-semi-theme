bb6d7c9d08ce595e958756c5902e172e42396123
andrew <andrew.devels@gmail.com>
2018-08-13 17:54:46 +0500

    Add UI Colors

---
+++ src/colors/ui/uiColorCollection.ts
@@ -0,0 +1,27 @@
+import UiColors from "./uiColors";
+import Color from "../color";
+import ShadeGenerator from "../utils/shadeGenerator";
+import UtilsManager from "../utils/utilsManager";
+
+function getSemiUi() {
+  const colors: UiColors = {
+    background: new Color("#1C282D"),
+    foreground: new Color("#D6D8D9"),
+    accent: new Color("#C62D42"),
+    border: new Color("#1A2226")
+  };
+
+  const manager = getDefautlManager();
+
+  manager.run(
+    ...[colors.background, colors.foreground, colors.accent, colors.border]
+  );
+
+  return colors;
+}
+
+function getDefautlManager() {
+  return new UtilsManager(new ShadeGenerator());
+}
+
+export const semiUi = getSemiUi();
---
+++ src/colors/ui/uiColors.d.ts
@@ -0,0 +1,8 @@
+import Color from "../color";
+
+export default interface UiColors {
+  background: Color;
+  foreground: Color;
+  accent: Color;
+  border: Color;
+}
